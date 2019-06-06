95b8778a9d15e3e96f1b2ae724df13d03ef38ffa
Yohan <yohancaro7@gmail.com>
2019-06-06 14:44:53 -0500

    Nuevos metodos simple list

--- src/co/uptc/structures/simple_list/MySimpleList.java
+++ src/co/uptc/structures/simple_list/MySimpleList.java
@@ -1,5 +1,6 @@
 package co.uptc.structures.simple_list;
 
+import java.lang.reflect.Array;
 import java.util.Comparator;
 
 /**
@@ -65,46 +66,7 @@ public class MySimpleList<T> {
     private void insert(T info) {
         this.head = new Node(info, this.head);
     }
-    
-    /**
-     * Search a element that is entered as parameter
-     * @param info Element to search
-     * @return Node of element
-     */
-    public Node<T> search(T info) {
-       	Node<T> aux = new Node<T>(info);
-    	if (head != null) {
-    		if (comparator.compare(head.info, info) == 0) { 
-                aux.info = info;
-            } else {
-            	aux = this.head.next;
-            	while (aux != null && comparator.compare(aux.info, info) != 0) {
-            		aux = aux.next;
-            	}
-            	if (aux != null) {
-            		aux = (comparator.compare(aux.info, info) == 0)? aux:null;
-            	}
-            }
-    	}
-    	return aux;
-    }
-    
-    /**
-     * Busca un objeto en la lista sin comparar
-     * @param info elemento a buscar
-     * @return aux elemento encontrado / o null si no
-     */
-    public Node<T> searchNoCompare(T info) {
-    	Node<T> aux = this.head;
-    	while (aux != null) {
-    		if (aux.info.equals(info)) {
-    			return aux;
-    		}
-    		aux = aux.next;
-    	}
-    	return null;
-    }
-    
+            
     /**
      * Metodo que añade un elemento a la lista!
      * @param info elemento a añadir.
@@ -159,11 +121,23 @@ public class MySimpleList<T> {
     	}
     }
     
-    /**
-     * Metodo que elimina un objeto de la lista!
+	/**
+	 * Elimina un elemento de la lista!
+	 * @param info dato
+	 */
+	public void remove(T info) {
+		if (this.comparator != null) {
+			this.removeCompare(info);
+		} else {
+			this.removeNoCompare(info);
+		}
+	}
+	
+	/**
+     * Metodo que elimina un objeto de la lista utilizando el comparador!
      * @param info elemento a eliminar.
      */
-    public void remove(T info) {
+    private void removeCompare(T info) {
         if (this.head != null) {
             if (comparator.compare(head.info, info) == 0) {
                 this.head = this.head.next;
@@ -179,8 +153,28 @@ public class MySimpleList<T> {
                 }
             }
         }
-
-        Node<T> aux = this.head;
+    }
+    
+	/**
+     * Metodo que elimina un objeto de la lista!
+     * @param info elemento a eliminar.
+     */
+    private void removeNoCompare(T info) {
+        if (this.head != null) {
+            if (this.head.info.equals(info)) {
+                this.head = this.head.next;
+            } else {
+                Node<T> aux = this.head.next;
+                Node<T> ant = this.head;
+                while (aux != null) {
+                    if (aux.info.equals(info)) {
+                        ant.next = aux.next;
+                    }
+                    ant = aux;
+                    aux = aux.next;
+                }
+            }
+        }
     }
     
     /**
@@ -201,8 +195,22 @@ public class MySimpleList<T> {
     	aux.info = infoNew;
     }
     
-    public T searchWithCompare(T info) {
-    	Node<T> aux = new Node<T>(info);
+    /**
+     * Busca un elemento en la lista
+     * @param info dato a buscar
+     * @return el dato buscado o null si no lo encuentra
+     */
+    public Node<T> search(T info) {
+    	return (this.comparator!=null)?this.searchCompare(info):this.searchNoCompare(info);
+    }
+    
+    /**
+     * Search a element that is entered as parameter
+     * @param info Element to search
+     * @return Node of element
+     */
+    private Node<T> searchCompare(T info) {
+       	Node<T> aux = new Node<T>(info);
     	if (head != null) {
     		if (comparator.compare(head.info, info) == 0) { 
                 aux.info = info;
@@ -216,11 +224,23 @@ public class MySimpleList<T> {
             	}
             }
     	}
-    	if (aux != null) {
-    		return aux.info;
-    	} else {
-    		return null;
-    	}
+    	return aux;
+    }
+
+    /**
+     * Busca un objeto en la lista sin comparar
+     * @param info elemento a buscar
+     * @return aux elemento encontrado / o null si no
+     */
+    private Node<T> searchNoCompare(T info) {
+        Node<T> aux = this.head;
+        while (aux != null) {
+            if (aux.info.equals(info)) {
+                return aux;
+            }
+            aux = aux.next;
+        }
+        return null;
     }
     
 	/**
@@ -240,6 +260,63 @@ public class MySimpleList<T> {
 		}
 		return list;
 	}
+	
+	/**
+	 * Añade los elementos de otra lista al comienzo 
+	 * @param list lista
+	 */
+	public void insertList(MySimpleList<T> list) {
+		if (!list.isEmpty()) {
+			Node<T> aux = list.head.next;
+			while (aux.next != null) {
+                aux = aux.next;
+            }
+			aux.next = this.head;
+			this.head = list.head;
+		}
+	}
+	
+	/**
+	 * Añade los elemtos de un array al final de la lista
+	 * @param array vector estatico
+	 */
+	public void addArray(T[] array) {
+		for (int i = 0; i < array.length; i++) {
+			this.addLast(array[i]);
+		}
+	}
+		
+	/**
+	 * Pasa los elementos de la lista a un array
+	 * @return array con datos
+	 */
+	@SuppressWarnings("unchecked")
+	public T[] toArray(Class<T> classT) {
+		Node<T> aux = this.head;
+		T[] array = (T[]) Array.newInstance(classT, this.size());
+		for (int i = 0; i < this.size(); i++, aux = aux.next) {
+			array[i] = aux.info;
+		}
+		return array;
+	}
+	
+    /**
+     * Give the size of the list
+     * @return a integer with the size
+     */
+    public int size() {
+    	if (head != null) {
+    		int cont = 0;
+    		Node<T> aux = this.head;
+    		while (aux != null) {
+    			aux = aux.next;
+    			cont++;
+    		}
+    		return cont;
+    	} else {
+    		return -1;
+    	}
+    }
 
     /**
      * Metodo que devuelve la cabeza!
